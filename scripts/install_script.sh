a1e87ddebea03ac28934399194192d5992f27ad3
daniel stan <daniel@layershift.me>
2017-04-28 14:45:18 +0100

    dummy skeleton for jps package

---
+++ scripts/install_script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo  "I got these params $1 $2 when installing"  >/tmp/openswan_install.txt
+touch /tmp/openswan.file
---
+++ scripts/uninstall_script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+echo  "I got these params $1 $2 when uninstalling" >>/tmp/openswan_install.txt 
+rm  -f /tmp/openswan.file 
